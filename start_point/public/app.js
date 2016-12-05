1d9c13878c5a8e552a3db1b3b258cfc92cf44e55
Max <mveasey87@gmail.com>
2016-12-05 20:52:37 +0000

    edited so all cats are edited in js not html

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -38,5 +38,8 @@ var makeNewCat = function(name, favFood, photo) {
 };
 
 window.onload = function() {
+  makeNewCat('Name: Boba', 'Favourite Food: Sock Fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg');
+  makeNewCat('Name: Barnaby', 'Favourite Food: Tuna', 'http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg');
+  makeNewCat('Name: Max', 'Favourite Food: Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg')
   makeNewCat('Name: Johnny D', 'Favourite Food: Grass', 'http://3.bp.blogspot.com/-bDvYRChttwM/Tv28XAwmloI/AAAAAAAApxo/csE7Ihi1M90/s1600/Popular+Breeds+-+004.jpg');
 };
