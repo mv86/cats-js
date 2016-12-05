d0de3c77fd7b142e7c526717a4d7af71d23c9456
Max <mveasey87@gmail.com>
2016-12-05 18:25:11 +0000

    refactored code to any new cat

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -1,15 +1,42 @@
-var makeUl = function () {
+var makeCatUl = function () {
   var newUl = document.createElement('ul');
-  newUl.classList.add('ul');
+  newUl.classList.add('cat');
   return newUl;
 };
 
 var catName = function(name) {
-  var name = document.createElement('li');
-  name.innerText = name;
-  return name;
+  var thisName = document.createElement('li');
+  thisName.innerText = name;
+  return thisName;
 };
 
-var catFavFood - function(favFood) {
-  
-}
+var catFavFood = function(favFood) {
+  var thisFood = document.createElement('li');
+  thisFood.innerText = favFood;
+  return thisFood;
+};
+
+var catPhoto = function(photo) {
+  var thisPhoto = document.createElement('img');
+  thisPhoto.src = photo;
+  thisPhoto.width = '500';
+  return thisPhoto;
+};
+
+var makeNewCat = function(name, favFood, photo) {
+  var catUl = makeCatUl();
+  var catsName = catName(name);
+  var catsFavFood = catFavFood(favFood);
+  var catsPhoto = catPhoto(photo);
+
+  catsFavFood.appendChild(catsPhoto);
+  catsName.appendChild(catsFavFood);
+  catUl.appendChild(catsName);
+
+  var catList = document.querySelector('#cats');
+  catList.append(catUl);
+};
+
+window.onload = function() {
+  makeNewCat('Name: Johnny D', 'Favourite Food: Grass', 'http://3.bp.blogspot.com/-bDvYRChttwM/Tv28XAwmloI/AAAAAAAApxo/csE7Ihi1M90/s1600/Popular+Breeds+-+004.jpg');
+};
