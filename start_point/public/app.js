967a864bb49687fcd5b9f2728b179b1f7b5baf38
Max <mveasey87@gmail.com>
2016-12-06 07:08:59 +0000

    all cat objects are now displayed on screen through an array

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -37,9 +37,20 @@ var makeNewCat = function(name, favFood, photo) {
   catList.append(catUl);
 };
 
-window.onload = function() {
-  makeNewCat('Name: Boba', 'Favourite Food: Sock Fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg');
-  makeNewCat('Name: Barnaby', 'Favourite Food: Tuna', 'http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg');
-  makeNewCat('Name: Max', 'Favourite Food: Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg')
-  makeNewCat('Name: Johnny D', 'Favourite Food: Grass', 'http://3.bp.blogspot.com/-bDvYRChttwM/Tv28XAwmloI/AAAAAAAApxo/csE7Ihi1M90/s1600/Popular+Breeds+-+004.jpg');
+var cat1 = {name: 'Name: Boba', favFood: 'Favourite Food: Sock Fluff', photo: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'};
+
+var cat2 = {name: 'Name: Barnaby', favFood: 'Favourite Food: Tuna', photo: 'http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg'};
+
+var cat3 = {name: 'Name: Max', favFood: 'Favourite Food: Whiskas Temptations', photo: 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'};
+
+var cat4 = {name: 'Name: Johnny D', favFood: 'Favourite Food: Grass', photo: 'http://3.bp.blogspot.com/-bDvYRChttwM/Tv28XAwmloI/AAAAAAAApxo/csE7Ihi1M90/s1600/Popular+Breeds+-+004.jpg'};
+
+var arrayOfCats = [cat1, cat2, cat3, cat4]
+
+var displayCats = function() {
+  for (cat of arrayOfCats) {
+    makeNewCat(cat.name, cat.favFood, cat.photo);
+  };
 };
+
+window.onload = displayCats;
